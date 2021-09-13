e8a5a36560cf77787d8a97806fa0d4a693cb0d5a
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2021-09-13 12:52:26 +0200

    Added typings

--- src/app/stats/stats.service.ts
+++ src/app/stats/stats.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Error, Player } from '../types';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,9 @@ import { Observable } from 'rxjs';
 export class StatsService {
   constructor(private http: HttpClient) {}
 
-  fetchData(tag: string): Observable<any> {
-    return this.http.get(`http://localhost:3000/players/${tag}`);
+  fetchData(tag: string): Observable<Player | Error> {
+    return this.http.get(`http://localhost:3000/players/${tag}`) as Observable<
+      Player | Error
+    >;
   }
 }
